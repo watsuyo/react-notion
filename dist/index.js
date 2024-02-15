9fedaed9e23d2fc6fbe008fe7cfaa64d5b8f47c4
watsuyo <watsuyo.dev@gmail.com>
2024-02-15 21:50:16 +0100

    Delete unnecessary files and update dependencies

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./react-notion.cjs.production.min.js')
+  module.exports = require('./react-notion-sushi.cjs.production.min.js')
 } else {
-  module.exports = require('./react-notion.cjs.development.js')
+  module.exports = require('./react-notion-sushi.cjs.development.js')
 }
